b0618a98fe9c45df2341377960bc98b252f61ec1
sunbin <769460962@qq.com>
2021-02-04 16:09:23 +0800

    爬虫 新闻

--- test2.py
+++ test2.py
@@ -1,54 +1,43 @@
 #!/usr/bin/python
-#-*- coding:UTF-8 -*-（添加）
+#-*- coding:UTF-8 -*-
 
 import requests
 from bs4 import BeautifulSoup
-import json
 
 NewCnt = 5
 
-def get_url(date):
+def get_url(date, category_list):
     url = 'http://www.chinanews.com/scroll-news/' + date +'/news.shtml'
     res = requests.get(url)
-    #res.encoding='GBK'  # html: ISO-8859-1 (2012)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')
 
     li_tag = soup.find('div','content_list').find_all('li')
-    category_list = []
-    title_list = []
-    url_list = []
+    news_dict = {}
     for li in li_tag:
         try:
             info = li.find_all('a')
             category = info[0].text
-            if category in [u'国内']:
-                category_list.append(category)
+            if category in category_list:
                 news_title = info[1].text
+                title_list = news_dict.get(category, [])
                 if len(title_list) == NewCnt:
-                    break
+                    continue
                 title_list.append(news_title)
-                news_url = 'http://www.chinanews.com'+str(info[1].get('href'))
-                url_list.append(news_url)
-                #print("have done!"+ news_title+":"+news_url)
+                news_dict[category] = title_list
         except:
             continue
-    # c = {
-    #     'class':category_list, #类别
-    #      'title':title_list,      #标题
-    #      'url':url_list
-    #      }
-    for k,v in enumerate(title_list):
-        print k+1,"、",v
-    #dic = json.dumps(c, ensure_ascii = False, encoding = 'utf-8')
-    #print(dic)
+
+    for key,value in news_dict.items():
+        print key
+        for k,v in enumerate(news_dict[key]):
+           print k+1,"、",v
 
 
 if __name__ == '__main__':
     date = "2021/0204"
     print date
-    print "国内要闻"
-    get_url(date)
+    get_url(date, [u'国内', u'娱乐'])
 
 
 
