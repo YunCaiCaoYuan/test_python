53485d93959303afde1da667ae6225d1f661325f
sunbin <769460962@qq.com>
2021-03-02 20:58:21 +0800

    test mock

---
+++ grammer/test_client.py
@@ -0,0 +1,18 @@
+import unittest
+import mock
+import client
+
+class TestClient(unittest.TestCase):
+
+    def test_success_request(self):
+        success_send = mock.Mock(return_value='200')
+        client.send_request = success_send
+        self.assertEqual(client.visit_ustack(), '200')
+
+    def test_fail_request(self):
+        fail_send = mock.Mock(return_value='404')
+        client.send_request = fail_send
+        self.assertEqual(client.visit_ustack(), '404')
+
+if __name__ == '__main__':
+    unittest.main()
