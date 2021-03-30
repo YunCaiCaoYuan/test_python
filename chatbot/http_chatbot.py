69b0374698a3147fc56dc55caf138fb20348858c
sunbin <769460962@qq.com>
2021-03-30 10:12:57 +0800

    todo 解析参数

--- chatbot/http_chatbot.py
+++ chatbot/http_chatbot.py
@@ -3,8 +3,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from chatbot.test_chatbot import get_response, init_chatbot
+import cgi
 import demjson
 
+# http.server不建议生产环境使用
 
 # data = {'result': 'this is a test'}
 
@@ -20,6 +22,15 @@ class Resquest(BaseHTTPRequestHandler):
         print("data=", data)
         self.wfile.write(json.dumps(data).encode())
     def do_POST(self):
+        # ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        # if ctype == 'multipart/form-data':
+        #     postvars = cgi.parse_multipart(self.rfile, pdict)
+        # elif ctype == 'application/x-www-form-urlencoded':
+        #     length = int(self.headers.getheader('content-length'))
+        #     postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        # else:
+        #     postvars = {}
+
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
